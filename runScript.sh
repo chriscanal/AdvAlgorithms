602548339c1d5666bf9087bb88a070f194a406f0
Christopher Canal <ccanal@Eta.coe.neu.edu>
2016-06-04 20:08:18 -0400

    Finished knapsack

---
+++ runScript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ampl knapsackRun/knapsackRun8.file > knapsackOutput/knapsack8.output
+ampl knapsackRun/knapsackRun12.file > knapsackOutput/knapsack12.output
+ampl knapsackRun/knapsackRun16.file > knapsackOutput/knapsack16.output
+ampl knapsackRun/knapsackRun20.file > knapsackOutput/knapsack20.output
+ampl knapsackRun/knapsackRun28.file > knapsackOutput/knapsack28.output
+ampl knapsackRun/knapsackRun32.file > knapsackOutput/knapsack32.output
+ampl knapsackRun/knapsackRun48.file > knapsackOutput/knapsack48.output
+ampl knapsackRun/knapsackRun64.file > knapsackOutput/knapsack64.output
+ampl knapsackRun/knapsackRun128.file > knapsackOutput/knapsack128.output
+ampl knapsackRun/knapsackRun256.file > knapsackOutput/knapsack256.output
+ampl knapsackRun/knapsackRun512.file > knapsackOutput/knapsack512.output
+ampl knapsackRun/knapsackRun1024.file > knapsackOutput/knapsack1024.output
+
